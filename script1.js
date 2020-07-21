5d477e483e874eb62e2ed57331f5552609c4473e
Tyler-999 <67513147+Tyler-999@users.noreply.github.com>
2020-07-20 20:13:23 -0400

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,10 @@
+// var firstName = prompt("What is your first name?")
+// var lastName = prompt("What is your last name?")
+// var age = prompt("How old are you?")
+
+// console.log ("Your full name is "+ firstName + " " + lastName);
+// console.log ("You are " + age + " years old");
+
+var age = prompt("How old are you?")
+var days = age * 365;
+alert(age + " years is roughly " + days + "days")
